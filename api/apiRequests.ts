493012a748319521c4720b0c5b87301310bd7f4f
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-09-10 01:31:15 +0900

    feat: make button select independently, remove toppins from sandwichTypes

--- api/apiRequests.ts
+++ api/apiRequests.ts
@@ -1,85 +1,19 @@
 import axios from "axios";
 import cheerio from "cheerio";
+import { info } from "console";
 import { infoType } from "../types/sandwich";
 import { BASE_URL, SANDWICH_URL, INGREDIENTS_URL } from "./APIURL";
 
-export const getSandwichInfoList = () => {
+const categories = ["bread", "vegetable", "cheese", "sauce"];
+
+export const getSandwichInfoList = async () => {
   const SANDWICH_FULL_URL = `${BASE_URL}${SANDWICH_URL}`;
-  axios(SANDWICH_FULL_URL)
-    .then((res) => {
-      const htmlData = res.data;
-      const $ = cheerio.load(htmlData);
-      const sandwichInfo: infoType[] = [];
-
-      $(".pd_list_wrapper li", htmlData).each((index, element) => {
-        const title = $(element).children(".eng").text();
-        const calories = $(element).children(".cal").text();
-        if (calories !== "") {
-          sandwichInfo.push({
-            title,
-            calories,
-          });
-        }
-      });
-      console.log(sandwichInfo);
-      return sandwichInfo;
-    })
-    .catch((err) => console.error(err));
-};
-
-// export const getIngredientCategoryList = () => {
-//   const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
-//   axios(CATEGORY_FULL_URL)
-//     .then((res) => {
-//       const htmlData = res.data;
-//       const $ = cheerio.load(htmlData);
-//       const ingredientsCategoryList: string[] = [];
-
-//       $(".pd_tab li", htmlData).each((index, element) => {
-//         const categoryTitle = $(element).text();
-//         if (categoryTitle !== "All")
-//           ingredientsCategoryList.push(categoryTitle);
-//       });
-//       console.log(ingredientsCategoryList);
-//       return ingredientsCategoryList;
-//     })
-//     .catch((err) => console.error(err));
-// };
-
-export const getBreadInfoList = () => {
-  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
-  axios(CATEGORY_FULL_URL)
-    .then((res) => {
-      const htmlData = res.data;
-      const $ = cheerio.load(htmlData);
-      const breadInfoList: infoType[] = [];
-
-      // attribs: {class: 'active'}
-      $(".pd_list_wrapper .bread", htmlData).each((index, element) => {
-        const title = $(element).children(".eng").text();
-        const calories = $(element).children(".cal").text();
-
-        if (calories !== "") {
-          breadInfoList.push({
-            title,
-            calories,
-          });
-        }
-      });
-
-      console.log(breadInfoList);
-    })
-    .catch((err) => console.error(err));
-};
-
-export const getVegetableInfoList = async () => {
-  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
-
-  const vegetable = await axios(CATEGORY_FULL_URL).then((res) => {
+  const infoList = await axios(SANDWICH_FULL_URL).then((res) => {
     const htmlData = res.data;
     const $ = cheerio.load(htmlData);
 
-    return $(".pd_list_wrapper .vegetable", htmlData).map((index, element) => {
+    return $(`.pd_list_wrapper li`, htmlData).map((index, element) => {
+      if (element.attribs.class === "ITEM_SANDWICH.TOPPING") return;
       const title = $(element).children(".eng").text();
       const calories = $(element).children(".cal").text();
       return {
@@ -88,8 +22,45 @@ export const getVegetableInfoList = async () => {
       };
     });
   });
-
-  return vegetable.toArray();
+  return infoList.toArray();
 };
 
-export const axiosFunc = () => {};
+export const getBreadInfoList = () => {
+  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
+  return getInfo(CATEGORY_FULL_URL, categories[0]);
+};
+
+export const getVegetableInfoList = async () => {
+  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
+  return getInfo(CATEGORY_FULL_URL, categories[1]);
+};
+
+export const getCheeseInfoList = async () => {
+  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
+  return getInfo(CATEGORY_FULL_URL, categories[2]);
+};
+
+export const getSauceInfoList = async () => {
+  const CATEGORY_FULL_URL = `${BASE_URL}${INGREDIENTS_URL}`;
+  return getInfo(CATEGORY_FULL_URL, categories[3]);
+};
+
+export const getInfo = async (URL: string, category: string) => {
+  const infoList = await axios(URL).then((res) => {
+    const htmlData = res.data;
+    const $ = cheerio.load(htmlData);
+
+    return $(`.pd_list_wrapper .${category}`, htmlData).map(
+      (index, element) => {
+        const title = $(element).children(".eng").text();
+        const calories = $(element).children(".cal").text();
+        return {
+          title,
+          calories,
+        };
+      }
+    );
+  });
+
+  return infoList.toArray();
+};
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,40 +1,104 @@
 import { Button, Text } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
-  getSandwichInfoList,
   getBreadInfoList,
+  getCheeseInfoList,
+  getSandwichInfoList,
+  getSauceInfoList,
   getVegetableInfoList,
 } from "../../api/apiRequests";
 import { infoType } from "../../types/sandwich";
 
 const HomeScreen = () => {
+  const [sandwichTypes, setSandwichTypes] = useState<infoType[]>([]);
+  const [breads, setBreads] = useState<infoType[]>([]);
   const [vegetables, setVegetables] = useState<infoType[]>([]);
+  const [sauces, setSauces] = useState<infoType[]>([]);
+  const [cheeses, setCheeses] = useState<infoType[]>([]);
+  const [isSelectedCategory, setIsSelectedCategory] = useState("sandwichTypes");
 
-  const handleClickSandwich = () => {
-    getSandwichInfoList();
+  const handleClickSandwich = async () => {
+    const sandwichType = await getSandwichInfoList();
+    setSandwichTypes(sandwichType);
+    setIsSelectedCategory("sandwichTypes");
   };
 
-  const handleClickBread = () => {
-    getBreadInfoList();
+  const handleClickBread = async () => {
+    const breads = await getBreadInfoList();
+    setBreads(breads);
+    setIsSelectedCategory("breads");
   };
 
   const handleClickVegetable = async () => {
-    const vegetable = await getVegetableInfoList();
-    setVegetables(vegetable);
+    const vegetables = await getVegetableInfoList();
+    setVegetables(vegetables);
+    setIsSelectedCategory("vegetables");
+  };
+
+  const handleClickSauce = async () => {
+    const sauces = await getSauceInfoList();
+    setSauces(sauces);
+    setIsSelectedCategory("sauces");
+  };
+
+  const handleClickCheese = async () => {
+    const cheeses = await getCheeseInfoList();
+    setCheeses(cheeses);
+    setIsSelectedCategory("cheeses");
   };
 
   return (
     <>
       <Button onClick={handleClickSandwich}>샌드위치 정보 버튼</Button>
       <Button onClick={handleClickBread}>빵 정보 버튼</Button>
+      <Button onClick={handleClickCheese}>치즈 정보 버튼</Button>
       <Button onClick={handleClickVegetable}>야채 정보 버튼</Button>
-      {vegetables.map((item: infoType) => {
-        return (
-          <Text key={item.title}>
-            {item.title} : {item.calories}
-          </Text>
-        );
-      })}
+      <Button onClick={handleClickSauce}>소스 정보 버튼</Button>
+
+      {isSelectedCategory === "sandwichTypes" &&
+        sandwichTypes.map((item: infoType) => {
+          return (
+            <Text key={item.title}>
+              {item.title} : {item.calories}
+            </Text>
+          );
+        })}
+
+      {isSelectedCategory === "breads" &&
+        breads.map((item: infoType) => {
+          return (
+            <Text key={`${item.title}${item.calories}`}>
+              {item.title} : {item.calories}
+            </Text>
+          );
+        })}
+
+      {isSelectedCategory === "cheeses" &&
+        cheeses.map((item: infoType) => {
+          return (
+            <Text key={item.title}>
+              {item.title} : {item.calories}
+            </Text>
+          );
+        })}
+
+      {isSelectedCategory === "vegetables" &&
+        vegetables.map((item: infoType) => {
+          return (
+            <Text key={item.title}>
+              {item.title} : {item.calories}
+            </Text>
+          );
+        })}
+
+      {isSelectedCategory === "sauces" &&
+        sauces.map((item: infoType) => {
+          return (
+            <Text key={item.title}>
+              {item.title} : {item.calories}
+            </Text>
+          );
+        })}
     </>
   );
 };
