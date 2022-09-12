91490e8a1063f67c23ba1897c894e0d8a91d3c25
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2022-09-12 19:42:47 +0900

    feat: bottom drawer added, accordion panel added

---
+++ components/AddedListAccordion.tsx
@@ -0,0 +1,35 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
+import React from "react";
+const AddedListAccordion = () => {
+  return (
+    <div>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Section 2 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default AddedListAccordion;
--- components/AddedListPopup.tsx
+++ components/AddedListPopup.tsx
@@ -1,6 +1,13 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import { infoType } from "../types/sandwich";
+import AddedListAccordion from "./AddedListAccordion";
 
 interface AddedListPopupProps {
   addedItems: infoType[];
@@ -9,6 +16,16 @@ interface AddedListPopupProps {
 const AddedListPopup: React.FC<AddedListPopupProps> = ({ addedItems }) => {
   return (
     <div>
+      <Drawer placement="bottom" isOpen={true} onClose={() => {}}>
+        <DrawerContent>
+          <AddedListAccordion />
+          <DrawerBody>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
       {addedItems.map((item) => {
         return (
           <div key={item.title}>
