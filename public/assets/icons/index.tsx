9c0767740c80c87d2495af5fcdab79f73dea2eab
tae100k <tkim3@oswego.edu>
2022-09-15 01:49:15 +0900

    fix: change png into svg, webpack added for svg

--- public/assets/icons/index.tsx
+++ public/assets/icons/index.tsx
@@ -1,5 +1,2 @@
-// TODO: png를 svg로 바꾸어야 함
-// export { default as HeaderLogo } from "./header-logo.svg";
-export { default as HeaderLogo } from "./header-logo.png";
-export { default as IngredientIcon } from "./ingredient-icon.png";
-// export { default as Layer } from "./layer.png";
+export { default as HeaderLogo } from "./header-logo.svg";
+export { default as IngredientIcon } from "./ingredient-icon.svg";
