e7bada831c5bfa345345870a353fdffa790864c9
tae100k <tkim3@oswego.edu>
2022-09-21 22:26:33 +0900

    feat: add icons

---
+++ public/assets/ingredients/SVG/index.tsx
@@ -0,0 +1,24 @@
+import FlatBread from "../SVG/bread/Flat Bread.svg";
+import HeartyItalian from "../SVG/bread/Hearty Italian.svg";
+import HoneyOat from "../SVG/bread/Honey Oat.svg";
+import ParmesanOregano from "../SVG/bread/Parmesan Oregano.svg";
+import Wheat from "../SVG/bread/Wheat.svg";
+import White from "../SVG/bread/White.svg";
+
+export const CustomSVG = {
+  Sandwich: {},
+  Size: {},
+  Bread: {
+    "flat bread": FlatBread,
+    "hearty italian": HeartyItalian,
+    "honey oat": HoneyOat,
+    "parmesan oregano": ParmesanOregano,
+    wheat: Wheat,
+    white: White,
+  },
+  Veggies: {},
+  Cheese: {},
+  Sauces: {},
+  Extras: {},
+  "Extra Cheese": {},
+};
