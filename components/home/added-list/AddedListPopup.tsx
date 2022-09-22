86ec696a003a6377ace6b816cc88a92375514a90
tae100k <tkim3@oswego.edu>
2022-09-23 00:32:12 +0900

    feat: box import deleted

--- components/home/added-list/AddedListPopup.tsx
+++ components/home/added-list/AddedListPopup.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import { Container, Flex, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { infoType } from "../../../types/sandwich";
 import AddedListAccordion from "./AddedListAccordion";
