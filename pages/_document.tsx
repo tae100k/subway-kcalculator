d89f819ae89a94777a3d3a9cd526fb080cda8c20
tae100k <tkim3@oswego.edu>
2022-10-09 01:31:16 +0900

    feat: favicon added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,17 +16,22 @@ class MyDocument extends Document {
     return initialProps;
   }
   render() {
-    return (
+  return (
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/icons/favicon.svg"
+          ></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin=""
           />
-          <link rel="shortcut icon" href="/favicon.svg" />
           <link
             href="https://fonts.googleapis.com/css2?family=Epilogue:wght@100;300;400;600;700;800&&display=swap"
             rel="stylesheet"
