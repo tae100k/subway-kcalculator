398148052742adf64260aab1bb5b25ab2ea42466
tae100k <tkim3@oswego.edu>
2022-10-08 14:48:32 +0900

    feat: unnecessary link removed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,34 +20,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="shortcut icon" href="/favicon.svg" />
-          <link
-            rel="preload"
-            href="/assets/video/burn-success.mp4"
-            as="video"
-            type="video/mp4"
-          />
-          <link
-            rel="preload"
-            href="/assets/video/burn-error.mp4"
-            as="video"
-            type="video/mp4"
-          />
-          <link
-            rel="preload"
-            href="/assets/video/burn-waiting.mp4"
-            as="video"
-            type="video/mp4"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin=""
-          />
-          <link
-            href="https://https://fonts.googleapis.com/css?family=Epilogue"
-            rel="stylesheet"
           />
         </Head>
         <body>
