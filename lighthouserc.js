26ad0b82a2529f3aee47b77b3f93dc526266e920
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-03-26 18:59:19 +0900

    feat: lighthouse test

---
+++ lighthouserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:3000"],
+      collect: {
+        numberOfRuns: 5,
+      },
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
+
+    upload: {
+      startServerCommand: "npm run start",
+      target: "temporary-public-storage",
+    },
+  },
+};
