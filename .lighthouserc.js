02420e431f9c8418182e89637f488680c262f03a
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-03-27 11:14:25 +0900

    feat: assert updated

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -12,7 +12,14 @@ module.exports = {
       },
     },
     assert: {
-      preset: "lighthouse:recommended",
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.1 }],
+
+        "categories:performance": ["warn", { minScore: 0.1 }],
+        "categories:accessibility": ["warn", { minScore: 0.1 }],
+        "categories:best-practices": ["warn", { minScore: 0.1 }],
+        "categories:seo": ["warn", { minScore: 0.1 }],
+      },
     },
     upload: {
       target: "temporary-public-storage",
