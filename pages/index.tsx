647aa586c25e988856e5020a7b54f7afd034209e
김태희 (Taehee Kim) <78027252+tae100k@users.noreply.github.com>
2024-06-23 18:40:36 +0900

    fix: env updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const apiUrl = process.env.SUBWAY_KCALCULATOR_API_URL;
+      const apiUrl = process.env.REACT_APP_SERVER_URL;
       console.log("apiUrl", apiUrl);
       try {
         const res = await fetch(`${apiUrl}/api/menu`, { cache: "no-store" });
